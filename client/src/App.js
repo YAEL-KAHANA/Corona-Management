8779b0e3c04dea47c5174d1bcdd5f314ef1a2428
YAEL-KAHANA <yael.kahana.2003@gmail.com>
2024-03-28 18:09:07 +0200

    deleted unnecesary line

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
     const [data, setData] = useState([]);
